fdda448170dbf7a170eafaabf334fd4b011b6bb7
ChrisRosadAmador <christopher.rosado.amador@gmail.com>
2024-12-31 16:27:21 -0800

    Add and center images for landing page

--- Frontend/src/LandingPage/LandingPage.tsx
+++ Frontend/src/LandingPage/LandingPage.tsx
@@ -1,28 +1,33 @@
 import PomodoroImg from "../assets/PomodoroTimer.jpg";
+import BlacklistImg from "../assets/blacklist-icon.jpeg";
+
 function LandingPage() {
   return (
     <>
       <h1 className="title-style">What is Anti-Doomscroll?</h1>
-      <div className="grid gap-2 grid-cols-2 justify-items-center" id="infoDiv">
-        <span className="border-red-500 justify-self-end border-4 Regular-text text-center w-1/2">
-          The Anti-Doomscroll website is a powerful tool to help students combat
-          procrastination. Built around the Pomodoro technique, it features a
-          timer paired with a Chrome extension to block access to blacklisted
-          websites, keeping your focus sharp and distractions at bay.
+      <div className="grid gap-5 grid-cols-2 pb-10" id="infoDiv">
+        <span className="justify-self-end Landing-Page-grid">
+          <p className="Regular-text text-left">
+            The Anti-Doomscroll website is a powerful tool to help students
+            combat procrastination. Built around the Pomodoro technique, it
+            features a timer paired with a Chrome extension to block access to
+            blacklisted websites, keeping your focus sharp and distractions at
+            bay.
+          </p>
         </span>
-        <span className="border-blue-500 justify-self-start border-4 Regular-text text-center w-1/2">
+        <span className="justify-self-start Landing-Page-grid">
           <img src={PomodoroImg} alt="Tomato pomodoro timer image"></img>
         </span>
-        <span className="border-green-500 justify-self-end my-10 border-4 Regular-text text-center w-1/2">
-          <img src={PomodoroImg} alt="Tomato pomodoro timer image"></img>
+        <span className="justify-self-end Landing-Page-grid">
+          <img src={BlacklistImg} alt="blacklist image"></img>
         </span>
-        <span className="border-orange-500 my-10 justify-self-start border-4 Regular-text text-center w-1/2">
-          The blacklist feature lets you create a list of websites that you want
-          to block or be notified about. If you try to visit any of these sites,
-          Anti-Doomscroll will step in—either blocking access or sending a
-          reminder to keep you on track. Of course, this only works if you grant
-          us the necessary permissions. Without them, you’re free to
-          procrastinate as you wish—good luck!
+        <span className="justify-self-start Landing-Page-grid">
+          <p className="text-left Regular-text">
+            The blacklist feature lets you block or receive reminders for
+            websites you want to avoid. With your permission, Anti-Doomscroll
+            steps in to either block access or nudge you back on track. Without
+            it, you're free to procrastinate—good luck!
+          </p>
         </span>
       </div>
     </>
